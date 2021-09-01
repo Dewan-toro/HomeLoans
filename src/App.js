7e710f91b59baddd0550567badc5e17987c28be8
Dewan-toro <dewantoro477@gmail.com>
2021-09-02 06:28:40 +0700

    added project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form, Button, Image, Header, Segment } from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkDmcO74Q05QZv5XQuAUn8uglR_fg4lc1gTg&usqp=CAU" size="medium" />
+      <Segment size="medium" style={{ width: "30vw", marginTop: "50px" }} raised>
+        <Header>Login</Header>
+        <Form>
+          <Form.Field>
+            <label>Email</label>
+            <input placeholder="Email address" type="email" />
+          </Form.Field>
+          <Form.Field>
+            <label>Password</label>
+            <input placeholder="Password" type="password" />
+          </Form.Field>
+          <Button fluid primary>
+            Login
+          </Button>
+        </Form>
+      </Segment>
     </div>
   );
 }
